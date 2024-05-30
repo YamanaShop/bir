4f01bb305085c3c90669c6052e4a7bdae00b5a6d
YamanaShop <159576695+YamanaShop@users.noreply.github.com>
2024-05-30 21:21:07 +0200

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/YamanaShop/bir
+
+go 1.22.2
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/YamanaShop/welpen"
+)
+
+func main() {
+	string1 := welpen.Bark()
+	string2 := welpen.Barks()
+	fmt.Println(string1)
+	fmt.Println(string2)
+	fmt.Println(welpen.Bark())
+	fmt.Println(welpen.Barks())
+	fmt.Println(welpen.BigBark())
+	fmt.Println(welpen.BigBarks())
+}
